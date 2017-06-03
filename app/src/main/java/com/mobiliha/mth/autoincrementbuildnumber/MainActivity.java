c51f8691e8a04a84b71c46c7f4078b2ed4c5b1e1
DESKTOP-3C86LBP\R.Tahmasebi <developerman.it@gmail.com>
2017-06-03 10:47:49 +0430

    first
    first

---
+++ app/src/main/java/com/mobiliha/mth/autoincrementbuildnumber/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobiliha.mth.autoincrementbuildnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "update", Toast.LENGTH_SHORT).show();
+    }
+}
